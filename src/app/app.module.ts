16392ee0e1e55bac51101c11142e455537d4e136
SohairMahmoud <sohairmahmoud333@gmail.com>
2022-05-17 03:16:15 +0200

    update project struture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,22 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReviewsComponent } from './reviews/reviews.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { AuthComponent } from './auth/auth.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ReviewsComponent,
+    ContactUsComponent,
+    TestimonialsComponent,
+    AboutUsComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,

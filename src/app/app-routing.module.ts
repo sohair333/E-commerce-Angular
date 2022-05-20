39b75a59213a82038a45156beaac03c1155692e2
SohairMahmoud <sohairmahmoud333@gmail.com>
2022-05-20 04:34:36 +0200

    add Auth Guard OF Authtication

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
+import { AuthGuard } from './auth/auth.guard';
 import { HomeComponent } from './home/home.component';
 import { DetailsProductsComponent } from './products/details-products/details-products.component';
 import { EditeProdustComponent } from './products/EditeProdust/edite-produst.component';
@@ -9,13 +10,13 @@ import { ProductsComponent } from './products/products.component';
 
 const approutes: Routes = [
   {path:'',redirectTo:'/home',pathMatch:'full'},
-  {path:'home',component:HomeComponent}, 
+  {path:'home',component:HomeComponent,canActivate:[AuthGuard]}, 
   {path:'products',component:ProductsComponent,children:[
     {path:'',component:ProductsComponent},
     {path:'new',component:EditeProdustComponent},
     {path:':id',component:DetailsProductsComponent},
     {path:':id/edit',component:EditeProdustComponent}
-  ]},
+  ],canActivate:[AuthGuard]},
   {path:'auth',component:AuthComponent}
 ];
 
---
+++ src/app/auth/auth.guard.ts
@@ -0,0 +1,28 @@
+import { Injectable } from "@angular/core";
+import {  ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from "@angular/router";
+import { map, Observable, tap ,take} from "rxjs";
+import { authService } from "./auth.service";
+
+@Injectable({
+    providedIn:'root'
+})
+export class AuthGuard implements CanActivate{
+    constructor(private Authservice:authService,private router :Router){}
+    canActivate(route :ActivatedRouteSnapshot,router :RouterStateSnapshot):boolean |Promise<boolean>|Observable<boolean>{
+
+       return this.Authservice.user.pipe(
+        take(1),   
+        map(
+           user =>{
+               return !!user;
+           }
+       ),tap( 
+        isAuth =>{
+         if(!isAuth){
+            this.router.navigate(['/auth']);
+         }
+       
+       }));
+    }
+
+}

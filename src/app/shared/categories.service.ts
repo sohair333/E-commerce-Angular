5b5dc21937bc27975b2f907affe5a53d5f89c0e1
SohairMahmoud <sohairmahmoud333@gmail.com>
2022-05-29 21:12:55 +0200

    fix bug categorie

--- src/app/shared/categories.service.ts
+++ src/app/shared/categories.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { AngularFireDatabase } from "@angular/fire/compat/database";
-
+import { map} from "rxjs";
 @Injectable({
     providedIn: 'root'
   })
@@ -9,6 +9,15 @@ export class CategoriesService{
 
     }
     getCategories(){
-        return this.angularFireDb.list('/categories');
+        return this.angularFireDb.list('/categories',(ref) => ref.orderByChild('name'))
+        .snapshotChanges()
+        .pipe(
+            map((actions:any) =>{
+                return actions.map((actions:any) =>({
+                    key:actions.key,
+                    val:actions.payload.val()
+                }));
+            })
+        );
     }
 }

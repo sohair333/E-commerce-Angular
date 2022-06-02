dcb4f5644eac7790cbdceaf19df7337b1fedfa71
SohairMahmoud <sohairmahmoud333@gmail.com>
2022-06-02 06:22:55 +0200

    filter products

--- src/app/Admin/admin-products/admin-products.component.ts
+++ src/app/Admin/admin-products/admin-products.component.ts
@@ -1,19 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { ProductService } from 'src/app/shared/product.service';
-
+import { Product } from 'src/app/models';
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
-  products$:any;
+export class AdminProductsComponent implements OnInit ,OnDestroy {
+  products$ !:Product[];
+  subscription !:Subscription;
+  filteredProducts !: any[];
 
   constructor(private pService:ProductService) { 
-    this.products$=this.pService.getAll().snapshotChanges();
+    this.subscription=this.pService.getAll().snapshotChanges().subscribe((prodcuts:any) => this.filteredProducts = this.products$ = prodcuts);
   }
 
   ngOnInit(): void {
+  }
+  filter(query :string){
+    console.log(query);
+    this.filteredProducts = (query) ?
+    this.products$.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) : this.products$;
+
+
+  }
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
   }
 
 }

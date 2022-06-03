1eed918d2dddd2ec359474096d4cc7ef27449375
SohairMahmoud <sohairmahmoud333@gmail.com>
2022-06-03 07:33:16 +0200

    add list filter product

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from 'src/app/shared/product.service';
+import { CategoriesService } from '../shared/categories.service';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
@@ -7,8 +8,11 @@ import { ProductService } from 'src/app/shared/product.service';
 })
 export class ProductsComponent implements OnInit {
   Products$ :any;
-  constructor(PService:ProductService) { 
+  categories$:any;
+  constructor(PService:ProductService,categoryService:CategoriesService) { 
     this.Products$ = PService.getAll();
+    this.categories$ = categoryService.getCategories();
+    
   }
 
   ngOnInit(): void {

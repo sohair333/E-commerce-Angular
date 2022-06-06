cd5028d1ac7e854c8040c82c3a364ae943b4902b
SohairMahmoud <sohairmahmoud333@gmail.com>
2022-06-06 19:09:19 +0200

    ui shopping cart

--- src/app/models/shopping-Cart.ts
+++ src/app/models/shopping-Cart.ts
@@ -3,7 +3,11 @@ import { ShoppingCartItems } from "./shopppingCart-items";
 
 export class ShoppingCart{
    
-    constructor(public  items:ShoppingCartItems[]){}
+    constructor(public  items:ShoppingCartItems[] |any ){}
+
+    get productIDS() {
+      return  Object.keys(this.items);
+    }
     get totalItemCount(){
         let Count =0;
         for(let ProductId in this.items){
--- src/app/shoppping-cart/shoppping-cart.component.ts
+++ src/app/shoppping-cart/shoppping-cart.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ShoppingCart } from '../models/shopping-Cart';
+import { ShoppingCartService } from '../shared/shopppingCart.service';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-shoppping-cart',
   templateUrl: './shoppping-cart.component.html',
   styleUrls: ['./shoppping-cart.component.css']
 })
 export class ShopppingCartComponent implements OnInit {
+  Cart$ !:Observable<ShoppingCart>;
+  constructor(private shopppingCartService:ShoppingCartService) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  async ngOnInit() {
+    this.Cart$  = await this.shopppingCartService.getCart();
   }
 
 }

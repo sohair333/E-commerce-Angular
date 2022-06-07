0c46a8855845dadceed27f313fe6b650347afefa
SohairMahmoud <sohairmahmoud333@gmail.com>
2022-06-07 09:36:01 +0200

    fix some issuses

--- src/app/models/shopppingCart-items.ts
+++ src/app/models/shopppingCart-items.ts
@@ -1,18 +1,16 @@
 import {  Product } from '../models';
 export class ShoppingCartItems{
 
-    // constructor(public product:Product,public quantity:number ){}
-
     key !: string;
     title !: string;
     imageUrl !: string;
     price!: number; 
+    categorie !:string;
     quantity!: number; 
   
     constructor(init?: Partial<ShoppingCartItems>) {
       Object.assign(this, init);
     }
-
 
 
     get totalPrice(){
--- src/app/shoppping-cart/shoppping-cart.component.ts
+++ src/app/shoppping-cart/shoppping-cart.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ShoppingCart } from '../models/shopping-Cart';
 import { ShoppingCartService } from '../shared/shopppingCart.service';
 import { Observable } from 'rxjs';
+import { pro } from '../models';
 @Component({
   selector: 'app-shoppping-cart',
   templateUrl: './shoppping-cart.component.html',
   styleUrls: ['./shoppping-cart.component.css']
 })
 export class ShopppingCartComponent implements OnInit {
+  
   Cart$ !:Observable<ShoppingCart>;
   constructor(private shopppingCartService:ShoppingCartService) { }
 

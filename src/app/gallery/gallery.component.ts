04cf43ddd68954bc5e7f7dffaeef7a4cb46802b0
SohairMahmoud <sohairmahmoud333@gmail.com>
2022-06-16 14:56:02 +0200

    finsih gallery comp

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit ,Input } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  AnimationEvent
+  
+} from '@angular/animations';
 interface Item{
   imageSrc:string;
   imageAlt:string;
@@ -6,7 +15,27 @@ interface Item{
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
-  styleUrls: ['./gallery.component.css']
+  styleUrls: ['./gallery.component.css'],
+  animations:[
+    trigger('animation',[
+      transition("void => visible",[
+        style({transform:"scale(0.5)"}),
+        animate('150ms',style({transform:'scale(1)'}))
+      ]),
+      transition('visible => void',[
+        style({transform:"scale(1)"}),
+        animate('150ms',style({transform:'scale(0.5)'}))
+      ]),
+
+    ]),
+    trigger('animation2',[
+      transition(':leave',[
+        style({opacity:1}),
+        animate('50ms',style({opacity:0.8}))
+      ])
+
+    ])
+  ]
 })
 export class GalleryComponent implements OnInit {
 
@@ -29,6 +58,28 @@ export class GalleryComponent implements OnInit {
     this.previewImage=true;
     this.currentIndex = index;
     this.currentLightBOXIMAGE = this.GalleryData[index];
+  }
+  onAnimationEnd(event:AnimationEvent){
+    if(event.toState === 'void'){
+      this.showMask = false;
+    }
+  }
+  onClosePreview(){
+    this.previewImage = false;
+  }
+  next(){
+    this.currentIndex = this.currentIndex+1;
+    if(this.currentIndex > this.GalleryData.length -1){
+      this.currentIndex = 0;
+    }
+    this.currentLightBOXIMAGE = this.GalleryData[this.currentIndex];
+  }
+  prev(){
+    this.currentIndex = this.currentIndex-1;
+    if(this.currentIndex < 0){
+      this.currentIndex = this.GalleryData.length -1;
+    }
+    this.currentLightBOXIMAGE = this.GalleryData[this.currentIndex];
   }
 
 }

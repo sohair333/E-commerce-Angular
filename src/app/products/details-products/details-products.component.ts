86b74555d7bc7f8a041351d9c11f250c01a372c8
SohairMahmoud <sohairmahmoud333@gmail.com>
2022-06-18 09:10:56 +0200

    ....

--- src/app/products/details-products/details-products.component.ts
+++ src/app/products/details-products/details-products.component.ts
@@ -3,13 +3,37 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-details-products',
   templateUrl: './details-products.component.html',
-  styleUrls: ['./details-products.component.css']
+  styleUrls: ['./details-products.component.css'],
 })
 export class DetailsProductsComponent implements OnInit {
+  images: any[] = [];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  UploadImages(event: any) {
+    let files = event.target.files;
+    let sliderLength= this.images.length + files.length;
+    if(sliderLength>5){
+      let limit = 5 - this.images.length;
+      for (let x = 0; x < limit; x++) {
+        let file = files[x];
+        let reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => {
+          this.images.push(reader.result);
+        };
+      }
+    }
+    else{
+      for (let x = 0; x < files.length; x++) {
+        let file = files[x];
+        let reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => {
+          this.images.push(reader.result);
+        };
+      }
+    }
+    
   }
-
 }
